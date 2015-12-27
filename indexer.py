d0499de91c2eb112e3ae86fa2e25666812cbdca3
lot9 <lordofthenines@gmail.com>
2015-12-27 14:29:23 -0500

    non-functional - migrating code to lxml

--- indexer.py
+++ indexer.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python
 
-import HTMLParser
-import urllib
+from lxml.html import parse
+from urllib2 import urlopen
+from pandas.io.parsers import TextParser
 
 CREATURE_BY_CR_URL = "http://www.d20pfsrd.com/bestiary/-bestiary-by-challenge-rating"
 
 url_text = []
 
-class MyHTMLParser(HTMLParser.HTMLParser):
-    def handle_data(self, data):
-        if data != '\n':
-            url_text.append(data)
-
 if __name__ == '__main__':
-    page_ref = urllib.urlopen(CREATURE_BY_CR_URL)
-    page_html = page_ref.read()
-
-    parser = MyHTMLParser()
-    parser.feed(page_html)
-    parser.close()
-
-    for element in url_text:
-        print element
+    parsed_html = parse(CREATURE_BY_CR_URL)
