d5832effecaabbd9d2924aa8ff6515ed41b81fb1
lot9 <lordofthenines@gmail.com>
2015-12-27 13:19:56 -0500

    indexer pulls entire contents of web page of interest

--- indexer.py
+++ indexer.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python
 
+import HTMLParser
 import urllib
 
 CREATURE_BY_CR_URL = "http://www.d20pfsrd.com/bestiary/-bestiary-by-challenge-rating"
 
-page_ref = urllib.urlopen(CREATURE_BY_CR_URL)
-page_html = page_ref.read()
+url_text = []
+
+class MyHTMLParser(HTMLParser.HTMLParser):
+    def handle_data(self, data):
+        if data != '\n':
+            url_text.append(data)
+
+if __name__ == '__main__':
+    page_ref = urllib.urlopen(CREATURE_BY_CR_URL)
+    page_html = page_ref.read()
+
+    parser = MyHTMLParser()
+    parser.feed(page_html)
+    parser.close()
+
+    for element in url_text:
+        print element
