7674c9a6e3d653ef3446842f35062c3b7b449a35
lot9 <lordofthenines@gmail.com>
2015-12-30 14:27:21 -0500

    initial indexer implementation complete

--- indexer.py
+++ indexer.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
+#
+#   This purpose of this script is to create a file containing an index of links to be used by
+#   other scripts.
+#
+#   More specifically, these links are absolute HTML paths to pages on the d20pfsrd that list 
+#   monster information from the Pathfinder RPG sorted by Challenge Rating.
+#
 
 from lxml.html import parse
 from urllib2 import urlopen
 from pandas.io.parsers import TextParser
 
+BASE_HREF = "http://www.d20pfsrd.com/"
 CREATURE_BY_CR_URL = "http://www.d20pfsrd.com/bestiary/-bestiary-by-challenge-rating"
 
-url_text = []
-
 if __name__ == '__main__':
+    out = open('indeces.txt', 'w')
+    
     parsed_html = parse(CREATURE_BY_CR_URL)
+    
+    doc = parsed_html.getroot()
+    links = doc.cssselect('.nav-toc-content ul li a')
+    
+    for link in links:
+        out.write(BASE_HREF + link.get('href') + '\n')
+    out.close()
