e721fb29b794a9f47324a958cab926ac88f3e40e
lot9 <lordofthenines@gmail.com>
2016-01-31 10:54:11 -0500

    refactored unit tests

--- data-mining/bestiary/tests/test_crawler.py
+++ data-mining/bestiary/tests/test_crawler.py
@@ -1,35 +1,38 @@
 '''A module that tests the basic functionality of functions and classes in the crawler module.'''
 
+
 import sys
 sys.path.append('..')
 
-from bestiary import crawler
 import unittest
+from lxml.html import parse
+from bestiary.core.creature import Creature
 
-class TestPFCreatureInfo(unittest.TestCase):
+
+class TestCreature(unittest.TestCase):
     '''This class tests the validity of crawler.PFCreatureInfo'''
+    LINK_PREFIX = 'http://www.d20pfsrd.com/bestiary/monster-listings/'
+    
+    def _test_update_name_and_cr(self, link, expected_name, expected_cr):
+        '''Executes a single, specific sanity check for Creature.update_name_and_cr(...)'''
+        # get root node of an HtmlElement tree representing the provided link
+        parsed_html = parse(link)
+        root = parsed_html.getroot()
+        # create a Crature object populated by information from the provided link
+        creature = Creature()
+        creature.update(root)
+        # check to see if the Creature object's attributes match expectations
+        self.assertEqual(creature.name, expected_name)
+        self.assertEqual(creature.cr, expected_cr)
+        
     def test_update_name_and_cr(self):
-        creature = crawler.PFCreatureInfo()
-        # Case 1 - Name and CR are in table header
-        creature.update('http://www.d20pfsrd.com/bestiary/monster-listings/aberrations/akata')
-        self.assertEqual(creature.name, 'Akata')
-        self.assertEqual(creature.cr, 'CR 1')
-        # Case 2 - Name and CR are in table element
-        creature.update('http://www.d20pfsrd.com/bestiary/monster-listings/oozes/amoeba-giant')
-        self.assertEqual(creature.name, 'Giant Amoeba')
-        self.assertEqual(creature.cr, 'CR 1')
-        # Case 3 - Name and CR are in table element and surrounded by <b> tag
-        creature.update('http://www.d20pfsrd.com/bestiary/monster-listings/vermin/ant/ant-giant-worker')
-        self.assertEqual(creature.name, 'Giant Ant Worker')
-        self.assertEqual(creature.cr, 'CR 1')
-        # Case 3 - Name and CR are in table element and surrounded by <font> tag
-        creature.update('http://www.d20pfsrd.com/bestiary/monster-listings/outsiders/elemental/elemental-ice/small-ice-elemental')
-        self.assertEqual(creature.name, 'Small Ice Elemental')
-        self.assertEqual(creature.cr, 'CR 1')
-        # Case 4 - Name and CR are in <th> and <td> elements, respectively
-        creature.update('http://www.d20pfsrd.com/bestiary/monster-listings/outsiders/demodand/demodand-tarry')
-        self.assertEqual(creature.name, 'Tarry Demodand')
-        self.assertEqual(creature.cr, 'CR 13')
+        '''Executes a small number of sanity checks for Creature.update_name_and_cr(...)'''
+        self._test_update_name_and_cr(self.LINK_PREFIX + 'aberrations/akata', 'Akata', 'CR 1')
+        self._test_update_name_and_cr(self.LINK_PREFIX + 'oozes/amoeba-giant', 'Giant Amoeba', 'CR 1')
+        self._test_update_name_and_cr(self.LINK_PREFIX + 'vermin/ant/ant-giant-worker', 'Giant Ant Worker', 'CR 1')
+        self._test_update_name_and_cr(self.LINK_PREFIX + 'outsiders/elemental/elemental-ice/small-ice-elemental', \
+                                          'Small Ice Elemental', 'CR 1')
+    
 
 if __name__ == '__main__':
     unittest.main()
